7925c011ddd5151675d2a806d7ab44802f7aeb28
Tiago Avila <tiago.avila09@gmail.com>
2019-09-02 19:24:28 -0300

    finished example with charts from code-maze

---
+++ SignalRExample/SignalRExampleServer/HubConfig/ChartHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRExampleServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRExampleServer.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data)
+        {
+            await Clients.All.SendAsync("broadcastchartdata", data);
+        }
+    }
+}
